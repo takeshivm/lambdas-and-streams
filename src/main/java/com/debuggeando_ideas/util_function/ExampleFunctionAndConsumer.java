a9b465cf235276edc0681d951928a7a5b9ee9ca6
takeshivm <vargasmurillotakeshi@gmail.com>
2024-06-10 18:50:33 -0500

    Function Interfaces Consumer and BiConsumer

--- src/main/java/com/debuggeando_ideas/util_function/ExampleFunctionAndConsumer.java
+++ src/main/java/com/debuggeando_ideas/util_function/ExampleFunctionAndConsumer.java
@@ -15,7 +15,7 @@ public class ExampleFunctionAndConsumer {
 
 
     public static void main(String[] args) {
-        String s1 = "Takeshi "; S
+        String s1 = "Takeshi ";
         String s2 = "Vargas";
 
         System.out.println(myEquals.test(s1,s2));

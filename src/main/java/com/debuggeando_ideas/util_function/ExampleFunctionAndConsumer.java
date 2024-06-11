5877d5efa473b36238f2f7e079532e211fa013d5
takeshivm <vargasmurillotakeshi@gmail.com>
2024-06-11 12:30:56 -0500

    Interface BiUnary Operator

--- src/main/java/com/debuggeando_ideas/util_function/ExampleFunctionAndConsumer.java
+++ src/main/java/com/debuggeando_ideas/util_function/ExampleFunctionAndConsumer.java
@@ -1,8 +1,7 @@
 package com.debuggeando_ideas.util_function;
 
 import java.util.*;
-import java.util.function.BiPredicate;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class ExampleFunctionAndConsumer {
 
@@ -12,6 +11,21 @@ public class ExampleFunctionAndConsumer {
     // INTERFACE SUPPLIER
     static Supplier<Integer> randomInt = () -> new Random().nextInt(100);
     static Supplier<MyProduct> productSupplier = () -> new MyProduct(1000, "Desktop PC");
+
+    // INTERFACE UNARY OPERATOR
+        //      static Function<String, String> funToUpper1 = s -> s.toUpperCase();
+        ///     CON REFERENCIA DE METODOS
+    static Function<String, String> funToUpper = String::toUpperCase;
+    static UnaryOperator<String> unaryToUpper = String::toUpperCase;
+
+    // INTERFACE UNARY OPERATOR, recibe el mismo argumento de entrada y de salida
+    // INTERFACE UNARY OPERATOR, EXTIENDE DE LA INTERFAZ FUNCTION
+
+    static UnaryOperator<Integer> int1 = n -> n * n;
+    static IntUnaryOperator int2 = n -> n * n;
+
+
+
 
 
 
@@ -38,6 +52,25 @@ public class ExampleFunctionAndConsumer {
         System.out.println(randomInt.get());
         System.out.println(randomInt.get());
         System.out.println(productSupplier.get());
+
+        // INTERFACE UNARYOPERATOR
+        String arg = "lambdas";
+
+        System.out.println(funToUpper.apply(arg));
+        System.out.println(unaryToUpper.apply(arg));
+
+        System.out.println(int1.apply(3));
+        System.out.println(int2.applyAsInt(2));
+
+        // INTERFACE BIUNARYOPERATOR, EXTIENDE DE LA INTERFAZ BIFUNCTION
+        // por lo tanto, vamos a recibir 2 argumentos
+        BiFunction<String, String, String> normalizerFunc = (a,b) -> a.toUpperCase() + " - " + b.toLowerCase();
+        System.out.println(normalizerFunc.apply("hola ","MUNDO"));
+
+        BinaryOperator<String> normalizer = (a,b) -> a.toUpperCase() + " - " + b.toLowerCase();
+        System.out.println(normalizer.apply("hola ","MUNDO"));
+
+
 
 
     }

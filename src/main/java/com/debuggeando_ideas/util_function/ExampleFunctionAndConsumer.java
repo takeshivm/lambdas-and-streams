a4c02a3135383ec95daa612d370812e9aec23955
takeshivm <vargasmurillotakeshi@gmail.com>
2024-06-11 11:40:02 -0500

    Supplier Interface

--- src/main/java/com/debuggeando_ideas/util_function/ExampleFunctionAndConsumer.java
+++ src/main/java/com/debuggeando_ideas/util_function/ExampleFunctionAndConsumer.java
@@ -1,17 +1,18 @@
 package com.debuggeando_ideas.util_function;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.function.BiPredicate;
+import java.util.function.Supplier;
 
 public class ExampleFunctionAndConsumer {
 
     // Function BIPREDICATE
     static BiPredicate<String, String> myEquals = (s1, s2) -> s1.concat(s2).equals("Takeshi Vargas");
 
-    // INTERFACE CONSUMER
+    // INTERFACE SUPPLIER
+    static Supplier<Integer> randomInt = () -> new Random().nextInt(100);
+    static Supplier<MyProduct> productSupplier = () -> new MyProduct(1000, "Desktop PC");
+
 
 
     public static void main(String[] args) {
@@ -20,15 +21,24 @@ public class ExampleFunctionAndConsumer {
 
         System.out.println(myEquals.test(s1,s2));
 
-
+        // INTERFACE CONSUMER
         Set<Integer> nums = Set.of(1,2,3,4,5,6);
         List<Integer> squares = new LinkedList<>();
 
         nums.forEach(n -> squares.add(n * n));
         System.out.println(squares);
 
+
+        // INTERFACE BICONSUMER
         Map<Boolean, String> maps = Map.of(true, "this is the True", false, "This is false");
 
         maps.forEach((k, v) -> System.out.println(k + " - " + v));
+
+        // INTERFACE SUPPLIER
+        System.out.println(randomInt.get());
+        System.out.println(randomInt.get());
+        System.out.println(productSupplier.get());
+
+
     }
 }
